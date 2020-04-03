8687596f2c7e8c4924b329e7783a40330343d207
Nenad <rakic84@gmail.com>
2020-04-03 08:33:31 +0200

    TC04

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,15 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("SignIn", () => {
+    cy.visit('https://react-redux.realworld.io/#/login?_k=t8cok8')
+    cy.title().should('eq', "Conduit")
+    cy.location('protocol').should('eq', 'https:')
+    cy.get('form').within(($form) => {
+        cy.get('[placeholder="Email"]').type('rakic84@gmail.com')
+        cy.get('[placeholder="Password"]').type('!@12qwaszx')
+        cy.root().submit()
+    })
+    cy.contains('Your Feed', { timeout: 10000 }).should('be.visible')
+})
