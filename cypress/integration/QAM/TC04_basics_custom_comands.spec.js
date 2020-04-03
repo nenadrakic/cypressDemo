6d114b669e096d8409a46db07ab4b344ce9e51af
Nenad <rakic84@gmail.com>
2020-04-03 08:55:09 +0200

    ProjectConfiguration

--- cypress/integration/QAM/TC04_basics_custom_comands.spec.js
+++ cypress/integration/QAM/TC04_basics_custom_comands.spec.js
@@ -1,5 +1,7 @@
 describe('Create and mark-unmark as favorite', () => {
 
+    Cypress.config('pageLoadTimeout', 100000) //this is for this class only
+
     before(() => {
         cy.SignIn()
     })
